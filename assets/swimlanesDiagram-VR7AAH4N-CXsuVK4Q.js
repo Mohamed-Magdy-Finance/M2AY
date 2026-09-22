e34c17cc816c49b5dc1ab95b2f27c51910ba5848
Mohamed Magdy <mohamedeones123@outlook.com>
2026-09-22 10:29:53 -0700

    UP_V3

---
+++ assets/swimlanesDiagram-VR7AAH4N-CXsuVK4Q.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BgosSKmw.js";import{_ as a}from"./MermaidDiagram-XpltWWya.js";import"./chunk-5VM5RSS4-D2zil5L1.js";import"./chunk-XXDRQBXY-DBlctQHv.js";import"./chunk-POPQ4Y6H-CWMujZhv.js";import"./chunk-F27PBJKO-B3ypPRHd.js";import"./channel-DcGROFdq.js";import"./ui-CDMlrOXe.js";import"./router-CqbapHZq.js";import"./index-CVGPln8I.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
